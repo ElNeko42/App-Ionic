2d0c825f1f66ed7b348d22e13e7786000228c8c2
elneko42 <franson177@gmail.com>
2023-12-30 17:44:08 +0100

    vista ciudad

--- src/app/city/city.page.ts
+++ src/app/city/city.page.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-city',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CityPage implements OnInit {
 
-  constructor() { }
+  id: any;
+  cities: any = [];
+  name: string = "";
+  description: string = ""; 
+  image: string = "";
+  constructor(
+    private http: HttpClient,
+    private activatedRouter: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.id = this.activatedRouter.snapshot.paramMap.get('id');
+    console.log("id", this.id);
+
+    this.getCities().subscribe(res => {
+      console.log("Res", res);
+      this.cities = res;
+      this.name = this.cities[this.id].name;
+      this.description = this.cities[this.id].desc;
+      this.image = this.cities[this.id].image;
+    })
+  }
+
+  getCities() {
+    return this.http.get("assets/files/cities.json")
+      .pipe(
+        map((res: any) => {
+          return res.data;
+        })
+      )
   }
 
 }
